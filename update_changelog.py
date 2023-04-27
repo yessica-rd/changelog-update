0aa2469e406e7010b3b5085e7c135bdd11f0cb3e
yessica-rd <yessica@leanmind.es>
2023-04-27 08:22:27 +0100

    Refactor: refactor update_changelog file

--- update_changelog.py
+++ update_changelog.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
+# This script will update the CHANGELOG.md file based on the commits in the pull request.
+# It will append a section the following template:
+#
+# - <Release START>
+# ## [X.Y.Z] - YYYY-MM-DD <- Release Header
+# ### [Category A] <-- From here until the last category is considered release content
+#      - [Commit Message]
+# ### [Category B]
+#      - [Commit Message]
+# - <Release END>
+# [... Previous Releases ...]
+#
+# [X.Y.Z]: github.com/owner/repository/compare/vX.Y.Z..vX.Y.Z <- Changelog Footer
+# [X.Y.Q]: github.com/owner/repository/compare/vX.Y.Z..vX.Y.Q
+
 import re
 import datetime
 import argparse
 import copy
-from typing import Tuple, Pattern, Dict, Literal
+from typing import Tuple, List, Dict
 from enum import Enum
 
 
@@ -13,175 +28,179 @@ class ChangeTypes(Enum):
     breaking = 'breaking'
 
 
+class ReleaseCategories(Enum):
+    added = 'Added'
+    changed = 'Changed'
+    fixed = 'Fixed'
+    removed = 'Removed'
+
+
 Version = Tuple[int, int, int]
 VersionToChangeTypeMap = Dict[ChangeTypes, Version]
 
 
-changelog_path = './CHANGELOG.md'
-
-
-def calculate_new_version(version: list[int], major=0, minor=0, patch=0) -> str:
-    version[0] += major
-    version[1] = 0 if major > 0 else version[1] + minor
-    version[2] = 0 if major > 0 or minor > 0 else version[2] + patch
-    return '.'.join(map(str, version))
-
-
-def define_arguments():
+def define_file_arguments():
     arguments_dictionary = {
         'repository_url': {'help': 'remote repository url'},
         'pull_request_commits': {'help': 'pull request commits'}
     }
     parser = argparse.ArgumentParser()
-    for key, value in arguments_dictionary.items():
-        parser.add_argument('--{}'.format(key), **value)
+    for arg_name, arg_value in arguments_dictionary.items():
+        parser.add_argument('--{}'.format(arg_name), **arg_value)
     return parser.parse_args()
 
 
-def get_change_type_from(commits_in_pull_requests: list[str]) -> ChangeTypes:
-    commits_starting_with_breaking = r'/breaking/.*$'
-    commits_starting_with_feat = r'/feat/.*$'
-
-    commits_containing_breaking_changes = [
-        commit for commit in commits_in_pull_requests if re.search(commits_starting_with_breaking, commit)
-    ]
-    if commits_containing_breaking_changes:
-        return ChangeTypes.breaking
-
-    commits_containing_feat_changes = [
-        commit for commit in commits_in_pull_requests if re.search(commits_starting_with_feat, commit)
-    ]
-    if commits_containing_feat_changes:
-        return ChangeTypes.feat
-
-    return ChangeTypes.fix
-
-
-def calcualte_new_version_based_on(previous_version: str, commits_in_pull_request: list[str]):
-    previous_version_as_list: list[int] = [
-        int(number) for number in previous_version.split('.')
-    ]
-    version_to_change_based_on_change_type: VersionToChangeTypeMap = {
-        ChangeTypes.breaking: (1, 0, 0),
-        ChangeTypes.feat: (0, 1, 0),
-        ChangeTypes.fix: (0, 0, 1)
+# builds a dictionary containing the formatted commits messages for each category
+def create_release_dictionary_from(commits: List[str]) -> dict[ReleaseCategories, List[str]]:
+    release_categories = {
+        ReleaseCategories.added.value: [],
+        ReleaseCategories.changed.value: [],
+        ReleaseCategories.fixed.value: [],
+        ReleaseCategories.removed.value: []
     }
-    change_type = get_change_type_from(commits_in_pull_request)
-    default_change_type = version_to_change_based_on_change_type[ChangeTypes.fix]
-    major, minor, patch = version_to_change_based_on_change_type.get(
-        change_type, default_change_type)
-    return calculate_new_version(previous_version_as_list, major, minor, patch)
-
-
-# Eg: ## [X.Y.Z] - YYYY-MM-DD
-def append_version_header_to(changelog_content: str, header_version_regex_common: Literal, new_version: str) -> str:
-    today = datetime.date.today().isoformat()
-    template = f'\n## [{new_version}] - {today}\n\n'
-    regex = re.compile(r'##\s' + header_version_regex_common + r'\s-')
-    index = re.search(regex, changelog_content).start()
-    changelog_content = \
-        changelog_content[:index - 1] + \
-        template + \
-        changelog_content[index:]
-    return changelog_content
-
-
-# Eg: [X.Y.Z]: github.com/owner/repository/compare/vX.Y.Z..vX.Y.Z
-def append_version_footer_to(changelog_content: str, header_version_regex_common: Literal, new_version: str, previous_version: str):
-    regex = re.compile(header_version_regex_common + r':\s*')
-    index = re.search(regex, changelog_content).start()
-    template = f'[{new_version}]: {file_arguments.repository_url}/compare/v{new_version}..v{previous_version}\n'
-    changelog_content = \
-        changelog_content[:index] + \
-        template + \
-        changelog_content[index:]
-    return changelog_content
-
-
-def get_headers_from_two_latest_versions(changelog_content: str, header_version_regex_common: Literal) -> tuple[str, str, str, str]:
-    regex = re.compile(r'##\s' + header_version_regex_common + r'\s-\s(\d{4}-\d{2}-\d{2})')
-    changelog_versions_headers = regex.findall(changelog_content)
-    new_version, new_version_date = changelog_versions_headers[0]
-    previous_version, previous_version_date = changelog_versions_headers[1]
-    return new_version, new_version_date, previous_version, previous_version_date
-
-
-def add_sections_to_changelog():
-    changelog_keys = {'Added', 'Changed', 'Fixed', 'Removed'}
-    last_version_content_dict = {key: [] for key in changelog_keys}
-    allowed_commits_messages = {
-        'ADD:': 'Added',
-        'FEAT:': 'Added',
-        'FEATURE:': 'Added',
-        'CHANGE:': 'Changed',
-        'REFACTOR:': 'Changed',
-        'FIX:': 'Fixed',
-        'REMOVE:': 'Removed'
+    commit_prefix_to_category_map = {
+        'ADD:': ReleaseCategories.added.value,
+        'FEAT:': ReleaseCategories.added.value,
+        'FEATURE:': ReleaseCategories.added.value,
+        'CHANGE:': ReleaseCategories.changed.value,
+        'REFACTOR:': ReleaseCategories.changed.value,
+        'FIX:': ReleaseCategories.fixed.value,
+        'REMOVE:': ReleaseCategories.removed.value,
     }
-    for message_prefix, key in allowed_commits_messages.items():
-        for commit in commits_from_pull_request:
-            if commit.upper().startswith(message_prefix):
-                message_text = commit.split(':', maxsplit=1)[1]
-                if message_text.strip().capitalize() not in last_version_content_dict[key]:
-                    last_version_content_dict[key].append(message_text.strip().capitalize())
-    return last_version_content_dict
+
+    for commit_prefix, category in commit_prefix_to_category_map.items():
+        for commit in commits:
+            if commit.upper().startswith(commit_prefix):
+                commit_message = commit.split(':', maxsplit=1)[1]
+                if commit_message.strip().capitalize() not in release_categories[category]:
+                    release_categories[category].append(commit_message.strip().capitalize())
+
+    return release_categories
 
 
-def convert_dictionary_to_string(dictionary: dict[str, list[str]]):
+def find_versions_from(changelog: str, commits: List[str]) -> Tuple[str, str]:
+    previous_version = find_previous_version_from(changelog)
+    new_version = find_new_version_from(previous_version, commits)
+    return new_version, previous_version
+
+
+def update_changelog(changelog: str, new_version: str, previous_version: str) -> str:
+    changelog = prepend_release_header_to(changelog, new_version)
+    changelog = append_footer_section_to(changelog, new_version, previous_version)
+    return changelog
+
+
+def transform_release_dict_to_formatted_string(release_section: dict[ReleaseCategories, List[str]]):
     content = ''
-    for key, values in sorted(last_version_content_dict.items()):
-        if values:
-            content += f'### {key}\n\n'
-            for value in values:
-                content += f'- {value}.\n'
+    for category, commits in sorted(release_section.items()):
+        if commits:
+            content += f'### {category}\n\n'
+            for commit_message in commits:
+                content += f'- {commit_message}.\n'
             content += '\n'
     return content
 
 
-def get_release_content_index(changelog_content: str) -> tuple[int, int]:
-    new_version, new_version_date, previous_version, previous_version_date = get_headers_from_two_latest_versions(
-            changelog_content, header_version_regex_common)
-    template_last_version = f'## [{new_version}] - {new_version_date}\n\n'
-    template_previous_version = f'## [{previous_version}] - {previous_version_date}\n\n'
-    starting_index = changelog_content.index(template_last_version) + len(template_last_version)
-    ending_index = changelog_content.index(template_previous_version)
+def get_release_content_index(changelog_content: str) -> Tuple[int, int]:
+    latest_release_header, second_latest_release_header = get_headers_from_two_latest_releases(changelog_content)
+    starting_index = changelog_content.index(latest_release_header) + len(latest_release_header)
+    ending_index = changelog_content.index(second_latest_release_header)
     return starting_index, ending_index
 
 
-with open(changelog_path, 'r') as changelog_file:
-    changelog_content_base = changelog_file.read()
+def find_previous_version_from(changelog: str) -> str:
+    # "## [X.Y.Z] -"
+    release_header_version_pattern = re.compile(r'##\s\[(\d+\.\d+\.\d+)\]\s-', re.MULTILINE)
+    return re.findall(release_header_version_pattern, changelog)[0]
 
-file_arguments = define_arguments()
-commits_from_pull_request = file_arguments.pull_request_commits.split('\n')
-changelog_content = copy.deepcopy(changelog_content_base)
-header_version_regex_common: Literal = r'\[(\d+\.\d+\.\d+)]'  # [X.Y.Z]
-new_version_header_regex = re.compile(
-    r'##\s' + header_version_regex_common + r'\s-', re.MULTILINE)
-previous_version = re.findall(new_version_header_regex, changelog_content)[0]
 
-new_version = calcualte_new_version_based_on(
-    previous_version, commits_from_pull_request)
-changelog_content = append_version_header_to(
-    changelog_content, header_version_regex_common, new_version)
-changelog_content = append_version_footer_to(
-    changelog_content, header_version_regex_common, new_version, previous_version)
-last_version_content_dict = add_sections_to_changelog()
+def find_new_version_from(previous_version: str, commits: List[str]) -> str:
+    version: List[int] = [int(number) for number in previous_version.split('.')]
+    version_to_change_based_on_change_type: VersionToChangeTypeMap = {
+        ChangeTypes.breaking.value: (1, 0, 0),
+        ChangeTypes.feat.value: (0, 1, 0),
+        ChangeTypes.fix.value: (0, 0, 1)
+    }
+    change_type = get_change_type_from(commits)
+    default_change_type = version_to_change_based_on_change_type[ChangeTypes.fix.value]
+    major, minor, patch = version_to_change_based_on_change_type.get(change_type, default_change_type)
 
-there_are_valid_commits = any(last_version_content_dict.values())
+    version[0] += major
+    version[1] = 0 if major > 0 else version[1] + minor
+    version[2] = 0 if major > 0 or minor > 0 else version[2] + patch
 
-if there_are_valid_commits:
-    last_commits_version_content_string = convert_dictionary_to_string(last_version_content_dict)
+    return '.'.join(map(str, version))
 
-    with open(changelog_path, 'w') as file:
-        starting_index, ending_index = get_release_content_index(changelog_content)
-        file.seek(starting_index)
-        file.seek(ending_index)
-        new_version_content_updated = \
-            changelog_content[:starting_index] + \
-            last_commits_version_content_string + \
-            changelog_content[ending_index:]
-        file.seek(0)
-        file.write(new_version_content_updated)
 
-    with open('VERSION', 'w') as version_file:
-        version_file.write(f'v{new_version}')
+def get_change_type_from(commits: List[str]) -> ChangeTypes:
+    commits_starting_with_breaking = r'/breaking/.*$'
+    commits_starting_with_feat = r'/feat/.*$'
+    commits_containing_breaking_changes = [commit for commit in commits if re.search(commits_starting_with_breaking, commit)]
+    commits_containing_feat_changes = [commit for commit in commits if re.search(commits_starting_with_feat, commit)]
+
+    if commits_containing_breaking_changes:
+        return ChangeTypes.breaking.value
+
+    if commits_containing_feat_changes:
+        return ChangeTypes.feat.value
+
+    return ChangeTypes.fix.value
+
+
+# Appends to the changelog the Release Header with the following format: ## [X.Y.Z] - YYYY-MM-DD
+def prepend_release_header_to(changelog: str, new_version: str) -> str:
+    # "## [X.Y.Z] -"
+    release_header_version_pattern = re.compile(r'##\s\[(\d+\.\d+\.\d+)\]\s-')
+    today = datetime.date.today().isoformat()
+    release_header_template = f'\n## [{new_version}] - {today}\n\n'
+    index = re.search(release_header_version_pattern, changelog).start()
+    changelog = changelog[:index - 1] + release_header_template + changelog[index:]
+    return changelog
+
+
+# Appends to the changelog the Footer section with the following format: [X.Y.Z]: github.com/owner/repository/compare/vX.Y.Z..vX.Y.Z
+def append_footer_section_to(changelog: str, new_version: str, previous_version: str):
+    # "[X.Y.Z]:"
+    footer_pattern = re.compile(r'\[(\d+\.\d+\.\d+)\]:\s*')
+    index = re.search(footer_pattern, changelog).start()
+    footer_template = f'[{new_version}]: {FILE_ARGUMENTS.repository_url}/compare/v{new_version}..v{previous_version}\n'
+    changelog = changelog[:index] + footer_template + changelog[index:]
+    return changelog
+
+
+def get_headers_from_two_latest_releases(changelog_content: str) -> Tuple[str, str]:
+    # "## [X.Y.Z] - YYYY-MM-DD"
+    release_header_pattern = re.compile(r'##\s\[(\d+\.\d+\.\d+)\]\s-\s(\d{4}-\d{2}-\d{2})')
+    changelog_versions_headers = release_header_pattern.findall(changelog_content)
+    new_version, new_version_date = changelog_versions_headers[0]
+    previous_version, previous_version_date = changelog_versions_headers[1]
+    latest_release_header = f'## [{new_version}] - {new_version_date}\n\n'
+    second_latest_release_header = f'## [{previous_version}] - {previous_version_date}\n\n'
+
+    return latest_release_header, second_latest_release_header
+
+
+with open('./CHANGELOG.md', 'r+') as changelog_file:
+    FILE_ARGUMENTS = define_file_arguments()
+    current_changelog = copy.deepcopy(changelog_file.read())
+    commits: List[str] = FILE_ARGUMENTS.pull_request_commits.split('\n')
+    release_section = create_release_dictionary_from(commits)
+    are_there_commits_to_include = any(release_section.values())
+
+    if are_there_commits_to_include:
+        new_version, previous_version = find_versions_from(current_changelog, commits)
+        current_changelog = update_changelog(current_changelog, new_version, previous_version)
+        release_section_as_string = transform_release_dict_to_formatted_string(release_section)
+        starting_index, ending_index = get_release_content_index(current_changelog)
+
+        changelog_file.seek(starting_index)
+        changelog_file.seek(ending_index)
+
+        updated_changelog = current_changelog[:starting_index] + release_section_as_string + current_changelog[ending_index:]
+
+        changelog_file.seek(0)
+        changelog_file.write(updated_changelog)
+        changelog_file.close()
+
+        with open('VERSION', 'w') as version_file:
+            version_file.write(f'v{new_version}')
